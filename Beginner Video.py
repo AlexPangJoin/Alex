608b5c8602439d7d659034655f466116be26c602
AlexPangJoin <xrpang26@uwcchina.org>
2026-09-04 13:22:25 +0800

    add nonsense

---
+++ Beginner Video.py
@@ -0,0 +1,53 @@
+# Print the age in different ways
+age = 16
+print("you are"+" "+str(age)+" years old")
+print("you are"+" "+str(age)+" years old")
+print(f"you are {age} years old")
+
+# Interger
+age = 25
+quantity = 10
+players = 2
+
+print(f"you are {age} years old")
+print(f"you have {quantity} items")
+print(f"there are {players} players")
+
+# Float
+gpa = 3.5
+distance = 12.7
+price = 19.99
+
+print(f"your GPA is {gpa}")
+print(f"the distance is {distance} miles")
+print(f"the price is ${price}")
+
+# String
+name = "John"
+city = "New York"
+food = "pizza"
+
+print(f"your name is {name}")
+print(f"you live in {city}")
+print(f"you like {food}")
+
+# Boolean
+Running = True
+Sleeping = False
+online = True
+
+print(f"Are you running? {Running}")
+print(f"Are you sleeping? {Sleeping}")
+print(f"Are you online? {online}")
+
+if Running:
+    print("You are running")
+else:
+    print("Good job, you are not running")
+
+# Variable
+x,y,z = 5, 10, 15
+print(f"x")
+print(f"y")
+print(f"z")
+
---
+++ Introduction to Python.py
@@ -0,0 +1,4 @@
+print("I like pizza")
+print("It's really good")
+
+hhh
