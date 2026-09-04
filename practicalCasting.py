c20a9af9d838f6c7ce7da4a41f546e37cb0d5be9
AlexPangJoin <xrpang26@uwcchina.org>
2026-09-04 14:25:08 +0800

    Change

--- practicalCasting.py
+++ practicalCasting.py
@@ -1,4 +1,34 @@
-Name = "John"
-age = 16
-student = True
-gpa = 3.5
+# Explicit Type Casting
+
+# name = "John"
+# age = 16
+# student = True
+# gpa = 3.5
+
+# print (type(name))
+# print (type(age))
+# print (type(student))
+# print (type(gpa))
+
+# age = float(age)
+# print (type(age))
+
+# gpa = int(gpa)
+# print (type(gpa))
+
+# student = str(student)
+# print (type(student))
+
+# name = str(name)
+# print (type(name))
+
+# age = bool(age)
+# print (age)
+
+# Implicit Type Casting
+
+x = 2
+y = 2.0
+
+x = x / y
+print (x)
