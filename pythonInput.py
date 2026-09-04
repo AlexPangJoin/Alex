660b3114000e261d74bff715c8e8335a35f68ddc
AlexPangJoin <xrpang26@uwcchina.org>
2026-09-04 14:30:12 +0800

    change

--- pythonInput.py
+++ pythonInput.py
@@ -1,8 +1,17 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-age = int(age)
-age = age + 1
+# name = input("Enter your name: ")
+# age = float(input("Enter your age: "))
+# age = age + 1
 
-print(f"Hello, {name}!")
-print(f"You are {age} years old.")
+# print(f"Hello, {name}!")
+# print(f"You are {age} years old.")
 
+adejective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adejective2 = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter one more adjective: ")
+
+print(f"Today, I went to a {adejective} zoo.")
+print(f"At the zoo, I saw {noun}.")
+print(f"{noun} was {adejective2} and {verb}ing.")
+print(f"It was a {adjective3} experience!")
