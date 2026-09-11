d10c636e4eb5711fe392f9c8fc254271ffcc45c2
AlexPangJoin <xrpang26@uwcchina.org>
2026-09-11 14:07:23 +0800

    new info

---
+++ MathinPython.py
@@ -0,0 +1,64 @@
+# friends = 1
+
+# friends += 2
+# friends -= 3
+# friends *= 4
+# friends /= 5
+# friends **= 6
+
+# print(friends)
+
+
+# x = 3.1415926535
+# y = -7
+# z = 9
+
+# result1 = round (x)
+# result2 = abs (y)
+# result3 =pow(z,3)
+# result4 = max(x,y,z)
+# result5 = min(x,y,z)
+
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# print(result5)
+
+
+# import math
+
+# x = 88.88
+
+# print(math.pi)
+# print(math.e)
+
+# result = math.sqrt(x)
+# result1 = math.ceil(x)
+# result2 = math.floor(x)
+
+# print(f"square root of {x} equal to {round(result,2)}")
+
+
+# import math
+
+# radio = float(input("Enter the radio of a circle: "))
+
+# circumference = radio * 2 * math.pi
+# print (f"The circumference is: {round(circumference,2)}cm")
+
+
+# import math
+
+# radio = float(input("Enter the radio of a circle: "))
+# area = math.pi * pow(radio,2)
+# print(f"The area of this circle is {round(area,2)}cm^2")
+
+
+import math
+
+a = float(input("enter the value of side A: "))
+b = float(input("enter the value of side B: "))
+
+c = math.sqrt(pow(a,2)+pow(b,2))
+print (f"side c = {round (c,2)}")
