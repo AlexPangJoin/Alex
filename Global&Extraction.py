3ccc28fb403d79dd814cf3452b334f1aefaf6acc
AlexPangJoin <xrpang26@uwcchina.org>
2026-09-22 11:01:45 +0800

    new

---
+++ Global&Extraction.py
@@ -0,0 +1,20 @@
+# x = 9
+
+# def calculate_cost():
+#     global x
+#     x += 1
+#     print (f"{x} cost")
+
+# calculate_cost()
+
+# for i in range(7):
+#     x = 3
+#     x += i
+#     print(x)
+
+S = "Programming"
+End = S [3:]
+Middle = S [3:7]
+
+print(End)
+print(Middle)
--- GlobalLocal.py
+++
@@ -1,8 +0,0 @@
-x = 9
-
-def calculate_cost():
-    global x
-    x += 1
-    print (f"{x} cost")
-
-calculate_cost()
